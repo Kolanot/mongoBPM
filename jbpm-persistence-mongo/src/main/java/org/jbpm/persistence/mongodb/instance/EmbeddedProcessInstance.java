a2167816a10c95d0dd1e16902872f642003ce51d
zhangjing2000 <zhangjing2000@yahoo.com>
2014-11-30 15:21:31 -0800

    Update EmbeddedProcessInstance.java
    remove blank comments

--- jbpm-persistence-mongo/src/main/java/org/jbpm/persistence/mongodb/instance/EmbeddedProcessInstance.java
+++ jbpm-persistence-mongo/src/main/java/org/jbpm/persistence/mongodb/instance/EmbeddedProcessInstance.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 
 public class EmbeddedProcessInstance implements Serializable {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	private long processInstanceId;
 
